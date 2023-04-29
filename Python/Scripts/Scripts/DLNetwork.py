Added feature visualization leaky relu slope scheduling. Fixed bug in feature visualization (image normalization)

Python/Scripts/Scripts/DLNetwork.py
@@ -10,7 +10,7 @@ from Scripts.ImageResource import ImageResource
 
 class DLNetwork(object):
     
-    def __init__(self, model, device, classification, input_size, softmax=False, export_path=os.path.join("export", "network")):
+    def __init__(self, model, device, classification, input_size, softmax=False, export_path=os.path.join("export", "network"), norm_mean=(0.,), norm_std=(1.,)):
         super().__init__()
         
         self.device = device
@@ -21,8 +21,25 @@ class DLNetwork(object):
             param.requires_grad = False
         self.model.eval()
 
+        # Testing
+        # with torch.no_grad():
+        #     a = torch.eye(224)
+        #     a = a.repeat(3,1,1)
+        #     a = a.to(self.device)
+        #     a = a.unsqueeze(0)
+        #     posteriors = self.model.forward(a)
+        #     print("posteriors:")
+        #     print(posteriors)
+
+        
+        if input_size[0] == 3 and len(norm_mean) == 1:
+            norm_mean = (norm_mean[0], norm_mean[0], norm_mean[0])
+            norm_std = (norm_std[0], norm_std[0], norm_std[0])
+        self.norm_mean = norm_mean
+        self.norm_std = norm_std
         self.feature_visualizer = FeatureVisualizer()
-        self.feature_visualizer.fv_settings.target_size = input_size
+        self.feature_visualizer.fv_settings.norm_mean = self.norm_mean
+        self.feature_visualizer.fv_settings.norm_std = self.norm_std
         self.module_dict = {}
 
         self.output_tracker_module_id = ""
@@ -103,19 +120,21 @@ class DLNetwork(object):
         module = self.module_dict[module_id]["module"]
         n_channels = self.module_dict[module_id]["size"][1]
         created_images, _ = self.feature_visualizer.visualize(self.model, module, self.device, image, n_channels)
-        out_images = self.feature_visualizer.export_transformation(created_images)
 
-        return out_images
+        return created_images
 
 
     def set_feature_visualization_params(self, param_dict):
         # Create new FeatureVisualizationParams instance and attach it to the feature visualizer. 
         fv_settings = FeatureVisualizationParams(**param_dict)
         fv_settings.mode = FeatureVisualizationParams.Mode(param_dict["mode"])
+        fv_settings.norm_mean = self.norm_mean
+        fv_settings.norm_std = self.norm_std
         self.feature_visualizer.set_fv_settings(fv_settings)
         # Change the pooling mode of the network.
-        self.model.embedded_model.set_tracked_pool_mode_(param_dict['pool_mode'])
-        self.model.embedded_model.resize_filter_to_mimic_poolstage_(param_dict['filter_mode'])
+        if param_dict['pool_mode'] != "undefined":
+            self.model.embedded_model.set_tracked_pool_mode_(param_dict['pool_mode'])
+        self.model.embedded_model.resize_filter_to_mimic_poolstage_(param_dict['mimic_poolstage_filter_size'])
 
 
     def get_classification_result(self):

Python/Scripts/Scripts/FeatureVisualizerRobust.py
@@ -24,7 +24,7 @@ class FeatureVisualizationParams(object):
     def __init__(self,
         mode=Mode.CENTERPIXEL,
         epochs=200,
-        epochs_without_robustness_transforms=0,
+        epochs_without_robustness_transforms=20,
         lr=20.,
         degrees=0,
         blur_sigma=0.5,
@@ -33,8 +33,9 @@ class FeatureVisualizationParams(object):
         slope_leaky_relu_scheduling=True,
         final_slope_leaky_relu = 0.01,
         pool_mode="avgpool", # has no effect but is listed here for completeness
-        filter_mode=False, # has no effect but is listed here for completeness
-
+        mimic_poolstage_filter_size=False, # has no effect but is listed here for completeness
+        norm_mean = (0.,),
+        norm_std=(1.,),
         ):
 
         self.mode = mode
@@ -47,6 +48,8 @@ class FeatureVisualizationParams(object):
         self.fraction_to_maximize = fraction_to_maximize
         self.slope_leaky_relu_scheduling = slope_leaky_relu_scheduling
         self.final_slope_leaky_relu = final_slope_leaky_relu
+        self.norm_mean = norm_mean
+        self.norm_std = norm_std
 
 
 class FeatureVisualizer(object):
@@ -62,7 +65,6 @@ class FeatureVisualizer(object):
         self.export_interval = export_interval
         self.export_path = export_path
         self.set_fv_settings(fv_settings)
-        self.export_transformation = ExportTransform()
 
     
     def set_fv_settings(self, feature_visualization_params):
@@ -74,14 +76,22 @@ class FeatureVisualizer(object):
         export_meta = []
         if channels is None:
             channels = np.arange(n_channels)
+        n_batches = int( np.ceil( n_channels / float(BATCHSIZE) ) )
         
-        init_image = init_image.to(device)
+        norm_mean = torch.tensor(self.fv_settings.norm_mean)
+        norm_mean = norm_mean.reshape((1, -1, 1, 1))
+        norm_std = torch.tensor(self.fv_settings.norm_std)
+        norm_std = norm_std.reshape((1, -1, 1, 1))
+
         if init_image.ndim==3:
             init_image = init_image.unsqueeze(0)
+        #init_image = (init_image - norm_mean) / norm_std # transform init_image from image space to network input space
+        init_image = init_image.to(device)
         
+        export_transformation = ExportTransform(norm_mean, norm_std)
+        distribution_regularizer = DistributionRegularizer()
         regularizer = self.create_regularizer(init_image.shape[2:])
         
-        n_batches = int( np.ceil( n_channels / float(BATCHSIZE) ) )
         
         print("Start gradient ascent on images")
         print("Feature visualization parameters:")
@@ -92,11 +102,9 @@ class FeatureVisualizer(object):
             channels_batch = channels[batchid * BATCHSIZE : (batchid + 1) * BATCHSIZE]
             n_batch_items = len(channels_batch)
             created_image = init_image.detach().clone().repeat(n_batch_items, 1, 1, 1)
-            if self.fv_settings.slope_leaky_relu_scheduling:
-                model.embedded_model.set_neg_slope_of_leaky_relus(1.)
-
             # LeakyReLU slope scheduling
             if self.fv_settings.slope_leaky_relu_scheduling:
+                model.embedded_model.set_neg_slope_of_leaky_relus(1.)
                 start_epoch = 0.25 * self.fv_settings.epochs
                 start_slope = 1.0
                 end_epoch = 0.75 * self.fv_settings.epochs
@@ -118,7 +126,7 @@ class FeatureVisualizer(object):
                 with torch.no_grad():
                     if epoch < self.fv_settings.epochs - self.fv_settings.epochs_without_robustness_transforms:
                         created_image = regularizer(created_image)
-                    created_image = created_image.clamp(-2., 2.)
+                    created_image = distribution_regularizer(created_image)
                 
                 created_image = created_image.detach()
                 created_image.requires_grad = True
@@ -157,7 +165,7 @@ class FeatureVisualizer(object):
 
                 loss.backward()
 
-                gradients = created_image.grad / (torch.sqrt((created_image.grad**2).sum((1,2,3), keepdims=True)) + 1e-6)
+                gradients = created_image.grad / (torch.sqrt((created_image.grad**2).sum((1,2,3), keepdim=True)) + 1e-6)
                 created_image = created_image - gradients * self.fv_settings.lr
 
                 if epoch % 20 == 0:
@@ -165,7 +173,7 @@ class FeatureVisualizer(object):
 
                 if self.export and (epoch % self.export_interval == 0 or epoch == self.epochs - 1):
                     with torch.no_grad():
-                        export_images = self.export_transformation(created_image.detach().cpu())
+                        export_images = export_transformation(created_image.detach())
                         if export_images.shape[1] != 3:
                             export_images = export_images.expand(-1, 3, -1, -1)
                         for i, channel in enumerate(channels_batch):
@@ -176,9 +184,10 @@ class FeatureVisualizer(object):
 
             created_image_aggregate.append(created_image.detach().cpu())
 
-        created_image = torch.cat(created_image_aggregate, 0)
+        created_image_aggregate = torch.cat(created_image_aggregate, 0)
+        created_image_aggregate = export_transformation(created_image_aggregate)
         
-        return created_image, export_meta
+        return created_image_aggregate, export_meta
 
 
     def create_regularizer(self, target_size):
@@ -192,17 +201,21 @@ class FeatureVisualizer(object):
 
 class ExportTransform(object):
 
-    def __call__(self, x):
-        if isinstance(x, torch.Tensor):
-            x = x.cpu().numpy()
+    def __init__(self, norm_mean, norm_std):
+        self.norm_mean = norm_mean
+        self.norm_std = norm_std
 
-        minimum = x.min((1,2,3), keepdims=True)
-        maximum = x.max((1,2,3), keepdims=True)
-        x = x - minimum
-        x = x / (maximum - minimum + 1e-6)
+    def __call__(self, x):
+        # x is on cpu
+        with torch.no_grad():
+            mean = x.mean((1,2,3), keepdim=True)
+            std = x.std((1,2,3), keepdim=True)
+            x = (x - mean) / std
+            x = x * self.norm_std + self.norm_mean
+            x = x.clamp(0., 1.)
+        x = x.numpy()
         x = x * 255
         x = x.astype(np.uint8)
-
         return x
 
 
@@ -215,9 +228,10 @@ class Regularizer(object):
             padding = int((degrees / 45.) * target_size[1] / (2. * np.sqrt(2.))) # approximately the size of the blank spots created by image rotation.
             rotation = transforms.RandomApply(torch.nn.ModuleList([
                 transforms.Pad(padding, padding_mode='edge'),
+                #transforms.Pad(padding, fill=0, padding_mode='constant'),
                 transforms.RandomRotation(degrees=degrees),
                 transforms.CenterCrop(target_size[1:]),
-                ]), p=0.3)
+                ]), p=0.2)
             transform_list.append(rotation)
 
         if blur_sigma > 0.:
@@ -228,7 +242,7 @@ class Regularizer(object):
         if roll > 0:
             rolling = transforms.RandomApply(torch.nn.ModuleList([
                 RandomRoll(roll),
-                ]), p=0.3)
+                ]), p=0.2)
             transform_list.append(rolling)
 
         self.transformation = transforms.Compose(transform_list)
@@ -243,11 +257,18 @@ class DistributionRegularizer(nn.Module):
         super().__init__()
 
     def forward(self, x):
-        # mean = x.mean((1,2,3), keepdims=True)
-        # std = x.std((1,2,3), keepdims=True)
-        # x_reg = (x - mean) / (std + 1e-6)
-        # x_new = ((1. - self.blend) * x) +  self.blend * x_reg
-        x = x.clamp(-2, 2)
+        # Transform to image space
+        # x = x * self.norm_std + self.norm_mean
+        # # x should in the range [0, 1]
+        # minimum = x.min(1, keepdim=True)[0].min(2, keepdim=True)[0].min(3, keepdim=True)[0]
+        # maximum = x.max(1, keepdim=True)[0].max(2, keepdim=True)[0].max(3, keepdim=True)[0]
+        # x = x - minimum
+        # x = x / (maximum - minimum + 1e-6)
+        # # Transform back to network input space
+        # x = (x - self.norm_mean) / self.norm_std
+        mean = x.mean((1,2,3), keepdim=True)
+        std = x.std((1,2,3), keepdim=True)
+        x = (x - mean) / std
         return x
 
 

Python/Scripts/Scripts/NoiseGenerator.py
@@ -13,6 +13,9 @@ class NoiseGenerator:
         self.active_noise_image = torch.rand(*self.shape, device=self.device)
 
         # self.active_noise_image = torch.zeros(*self.shape, device=self.device)
+        # self.active_noise_image[0, 1::4, 1::4] = 1.
+        # self.active_noise_image[1, 2::4, 2::4] = 1.
+        # self.active_noise_image[2, 3::4, 3::4] = 1.
         # self.active_noise_image[:, self.shape[1]//2, self.shape[2]//2] = 1.
 
         # self.active_noise_image = torch.zeros(*self.shape, device=self.device)
@@ -22,4 +25,4 @@ class NoiseGenerator:
     def get_noise_image(self):
         if self.active_noise_image is None:
             self.generate_noise_image()
-        return self.active_noise_image
+        return self.active_noise_image.clone()

Python/Scripts/Scripts/PredefinedFilterModules.py
@@ -416,6 +416,38 @@ class DifferentiableClamp(torch.autograd.Function):
         return grad_output, None, None
 
 
+class EliminateNegGradients(torch.autograd.Function):
+    """
+    In the forward pass this operation behaves like identity.
+    But in the backward pass it applies relu on gradients.
+    """
+
+    @staticmethod
+    def forward(ctx, input):
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return torch.clamp(grad_output, min=0.)
+
+
+class PrefactorNegGradients(torch.autograd.Function):
+    """
+    In the forward pass this operation behaves like identity.
+    But in the backward pass it applies relu on gradients.
+    """
+
+    @staticmethod
+    def forward(ctx, input):
+        return input
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        mask = grad_output < 0.
+        grad_output[mask] = grad_output[mask] * 0.9
+        return grad_output
+    
+
 class NormalizationModule(nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -659,17 +691,27 @@ class PredefinedConv(nn.Module):
         self.tracker_out.register_data("PFModule_kernels", self.weight)
 
 
+    def get_internal_weights(self):
+        n_channels_per_kernel = self.n_channels_out // self.n_kernels
+        return self.weight.data.repeat((n_channels_per_kernel, 1, 1, 1))
+
     def update_internal_weights(self):
         # The internal weights have to be recomputed when the weights have been changed.
-        n_channels_per_kernel = self.n_channels_out // self.n_kernels
-        self.internal_weight.data = self.weight.data.repeat((n_channels_per_kernel, 1, 1, 1))
+        self.internal_weight.data = self.get_internal_weights()
     
 
     def forward(self, x: Tensor) -> Tensor:
+        if self.training:
+            # recompute the internal weights because we need the gradient to propagate back to the weights
+            internal_weight = self.get_internal_weights()
+            self.internal_weight.data = internal_weight
+        else:
+            internal_weight = self.internal_weight
+            
         groups = self.n_channels_in
         # if self.padding > 0:
         #     x = F.pad(x, (self.padding,self.padding,self.padding,self.padding), "replicate")
-        out = F.conv2d(x, self.internal_weight, None, self.stride, groups=groups, dilation=self.dilation, padding=self.padding)
+        out = F.conv2d(x, internal_weight, None, self.stride, groups=groups, dilation=self.dilation, padding=self.padding)
 
         _ = self.tracker_out(out)
         #print(f"multadds {x.shape[2]*x.shape[3]*self.n_channels_out*self.weight.shape[1]*self.weight.shape[2]}")
@@ -817,7 +859,10 @@ class ParamTranslationModule(WeightRegularizationModule):
 
     def get_internal_weights(self):
         # The internal weights have to be recomputed after any change of the weights
-        internal_weight = - torch.abs(self.weight - self.kernel_positions) / self.step_size + 1.
+        distances = torch.abs(self.weight - self.kernel_positions) / self.step_size
+        #distances = EliminateNegGradients.apply(distances)
+        distances = PrefactorNegGradients.apply(distances)
+        internal_weight = - distances + 1.0
         internal_weight[internal_weight < -self.gradient_radius] = 0.
         internal_weight = DifferentiableClamp.apply(internal_weight, 0., 1.)
         return internal_weight
@@ -827,9 +872,12 @@ class ParamTranslationModule(WeightRegularizationModule):
 
     def forward(self, x):
         if self.training:
+            # recompute the internal weights because we need the gradient to propagate back to the weights
             internal_weight = self.get_internal_weights()
+            self.internal_weight.data = internal_weight
         else:
             internal_weight = self.internal_weight
+
         out = F.conv2d(x, internal_weight, groups=x.shape[1])
         _ = self.tracker_out(out)
         return out